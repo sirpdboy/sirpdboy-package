7d1d6a96035a3699a63da515b2abf3859f7737b0
sirpdboy <sirpdboy@qq.com>
2020-10-07 22:54:34 +0800

    up wman3 netdata

--- luci-app-netdata/luasrc/controller/netdata.lua
+++ luci-app-netdata/luasrc/controller/netdata.lua
@@ -7,12 +7,6 @@ function index()
 	local fs = require "nixio.fs"
 
 	entry({"admin","status","netdata"},template("netdata"),_("NetData"),10).leaf=true
-	entry({"admin", "status", "netdata", "call"}, post("action_netdata"))	
 
 
-end
-
-function action_netdata()
-	luci.sys.call("/usr/share/netdatacn/netdatacn.sh 2>&1 >/dev/null")
-	luci.http.redirect(luci.dispatcher.build_url("admin","status","netdata"))
 end
