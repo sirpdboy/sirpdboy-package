99debc3bbf030d321c9c777b4b292603d438b0e7
sirpdboy <sirpdboy@qq.com>
2020-11-05 23:04:19 +0800

    proxy

---
+++ luci-app-ddnsto/luasrc/controller/ddnsto.lua
@@ -0,0 +1,14 @@
+module("luci.controller.ddnsto",package.seeall)
+function index()
+if not nixio.fs.access("/etc/config/ddnsto")then
+return
+end
+entry({"admin","services","ddnsto"},cbi("ddnsto/global"),_("ddnsto"),5).dependent=true
+entry({"admin","services","ddnsto","status"},call("act_status")).leaf=true
+end
+function act_status()
+local e={}
+e.ddnsto=luci.sys.call("pgrep /usr/bin/ddnsto >/dev/null")==0
+luci.http.prepare_content("application/json")
+luci.http.write_json(e)
+end
