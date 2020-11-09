38a3cfa9ad6bd01884198aaaf04e6e7504f128cc
sirpdboy <sirpdboy@qq.com>
2020-11-10 01:12:46 +0800

    up  advanced

--- luci-app-advanced/luasrc/model/cbi/advanced.lua
+++ luci-app-advanced/luasrc/model/cbi/advanced.lua
@@ -1,12 +1,12 @@
 local e=require"nixio.fs"
 local t=require"luci.sys"
-m=Map("advanced",translate("网络设置"),translate("<br /><font color=\"Red\"><strong>配置文档是直接编辑的！除非你知道自己在干什么，否则请不要轻易修改这些配置文档。配置不正确可能会导致不能开机等错误。</strong></font><br/>"))
+m=Map("advanced",translate("网络设置"),translate("<font color=\"Red\"><strong>配置文档是直接编辑的！除非你知道自己在干什么，否则请不要轻易修改这些配置文档。配置不正确可能会导致不能开机等错误。</strong></font><br/>"))
 m.apply_on_parse=true
 s=m:section(TypedSection,"advanced")
 s.anonymous=true
 if nixio.fs.access("/etc/dnsmasq.conf")then
 
-s:tab("dnsmasqconf",translate("配置dnsmasq"),translate("本页是配置/etc/dnsmasq.conf的文档内容。应用保存后自动重启生效"))
+s:tab("dnsmasqconf",translate("配置dnsmasq"),translate("本页是配置/etc/dnsmasq.conf的文档内容。应用保存后自动重启生效！"))
 
 conf=s:taboption("dnsmasqconf",Value,"dnsmasqconf",nil,translate("开头的数字符号（＃）或分号的每一行（;）被视为注释；删除（;）启用指定选项。"))
 conf.template="cbi/tvalue"
@@ -28,7 +28,7 @@ end
 end
 end
 if nixio.fs.access("/etc/config/network")then
-s:tab("netwrokconf",translate("配置网络"),translate("本页是配置/etc/config/network的文档内容。应用保存后自动重启生效"))
+s:tab("netwrokconf",translate("配置网络"),translate("本页是配置/etc/config/network的文档内容。应用保存后自动重启生效！"))
 
 conf=s:taboption("netwrokconf",Value,"netwrokconf",nil,translate("开头的数字符号（＃）或分号的每一行（;）被视为注释；删除（;）启用指定选项。"))
 conf.template="cbi/tvalue"
@@ -50,7 +50,7 @@ end
 end
 end
 if nixio.fs.access("/etc/hosts")then
-s:tab("hostsconf",translate("配置hosts"),translate("本页是配置/etc/hosts的文档内容。应用保存后自动重启生效"))
+s:tab("hostsconf",translate("配置hosts"),translate("本页是配置/etc/hosts的文档内容。应用保存后自动重启生效！"))
 
 conf=s:taboption("hostsconf",Value,"hostsconf",nil,translate("开头的数字符号（＃）或分号的每一行（;）被视为注释；删除（;）启用指定选项。"))
 conf.template="cbi/tvalue"
@@ -72,7 +72,7 @@ end
 end
 end
 if nixio.fs.access("/etc/config/dhcp")then
-s:tab("dhcpconf",translate("配置DHCP"),translate("本页是配置/etc/config/DHCP的文档内容。应用保存后自动重启生效"))
+s:tab("dhcpconf",translate("配置DHCP"),translate("本页是配置/etc/config/DHCP的文档内容。应用保存后自动重启生效！"))
 conf=s:taboption("dhcpconf",Value,"dhcpconf",nil,translate("开头的数字符号（＃）或分号的每一行（;）被视为注释；删除（;）启用指定选项。"))
 conf.template="cbi/tvalue"
 conf.rows=20
@@ -93,7 +93,7 @@ end
 end
 end
 if nixio.fs.access("/etc/config/firewall")then
-s:tab("firewallconf",translate("配置防火墙"),translate("本页是配置/etc/config/firewall的文档内容。应用保存后自动重启生效"))
+s:tab("firewallconf",translate("配置防火墙"),translate("本页是配置/etc/config/firewall的文档内容。应用保存后自动重启生效!"))
 
 conf=s:taboption("firewallconf",Value,"firewallconf",nil,translate("开头的数字符号（＃）或分号的每一行（;）被视为注释；删除（;）启用指定选项。"))
 conf.template="cbi/tvalue"
@@ -115,7 +115,7 @@ end
 end
 end
 if nixio.fs.access("/etc/config/mwan3")then
-s:tab("mwan3conf",translate("配置负载均衡"),translate("本页是配置/etc/config/mwan3的文档内容。应用保存后自动重启生效"))
+s:tab("mwan3conf",translate("配置mwan3"),translate("本页是配置/etc/config/mwan3的文档内容。应用保存后自动重启生效!"))
 
 conf=s:taboption("mwan3conf",Value,"mwan3conf",nil,translate("开头的数字符号（＃）或分号的每一行（;）被视为注释；删除（;）启用指定选项。"))
 conf.template="cbi/tvalue"
@@ -137,7 +137,7 @@ end
 end
 end
 if nixio.fs.access("/etc/config/ddns")then
-s:tab("ddnsconf",translate("配置ddns"),translate("本页是配置/etc/config/ddns"))
+s:tab("ddnsconf",translate("配置ddns"),translate("本页是配置/etc/config/ddnsr的文档内容。应用保存后自动重启生效！"))
 conf=s:taboption("ddnsconf",Value,"ddnsconf",nil,translate("开头的数字符号（＃）或分号的每一行（;）被视为注释；删除（;）启用指定选项。"))
 conf.template="cbi/tvalue"
 conf.rows=20
@@ -157,8 +157,29 @@ e.remove("/tmp/ddns")
 end
 end
 end
+if nixio.fs.access("/etc/config/fstab")then
+s:tab("fstab",translate("配置fstab"),translate("本页是配置/etc/config/fstab磁盘挂载配置文档内容。应用保存后自动重启生效！"))
+conf=s:taboption("fstabconf",Value,"fstabconf",nil,translate("开头的数字符号（＃）或分号的每一行（;）被视为注释；删除（;）启用指定选项。"))
+conf.template="cbi/tvalue"
+conf.rows=20
+conf.wrap="off"
+conf.cfgvalue=function(t,t)
+return e.readfile("/etc/config/fstab")or""
+end
+conf.write=function(a,a,t)
+if t then
+t=t:gsub("\r\n?","\n")
+e.writefile("/tmp/fstab",t)
+if(luci.sys.call("cmp -s /tmp/fstab /etc/config/fstab")==1)then
+e.writefile("/etc/config/fstab",t)
+luci.sys.call("/etc/init.d/fstab restart >/dev/null")
+end
+e.remove("/tmp/fstab")
+end
+end
+end
 if nixio.fs.access("/etc/config/smartdns")then
-s:tab("smartdnsconf",translate("配置smartdns"),translate("本页是配置/etc/config/smartdns的文档内容。应用保存后自动重启生效"))
+s:tab("smartdnsconf",translate("配置smartdns"),translate("本页是配置/etc/config/smartdns的文档内容。应用保存后自动重启生效！"))
 conf=s:taboption("smartdnsconf",Value,"smartdnsconf",nil,translate("开头的数字符号（＃）或分号的每一行（;）被视为注释；删除（;）启用指定选项。"))
 conf.template="cbi/tvalue"
 conf.rows=20
@@ -179,7 +200,7 @@ end
 end
 end
 if nixio.fs.access("/etc/config/openclash")then
-s:tab("openclashconf",translate("配置openclash"),translate("本页是配置/etc/config/openclash的文档内容。应用保存后自动重启生效"))
+s:tab("openclashconf",translate("配置openclash"),translate("本页是配置/etc/config/openclash的文档内容。应用保存后自动重启生效！"))
 conf=s:taboption("openclashconf",Value,"openclashconf",nil,translate("开头的数字符号（＃）或分号的每一行（;）被视为注释；删除（;）启用指定选项。"))
 conf.template="cbi/tvalue"
 conf.rows=20
@@ -199,29 +220,8 @@ e.remove("/tmp/openclash")
 end
 end
 end
-if nixio.fs.access("/etc/config/AdGuardHome")then
-s:tab("AdGuardHomeconf",translate("配置AdGuardHome"),translate("本页是配置/etc/config/AdGuardHome的文档内容。应用保存后自动重启生效"))
-conf=s:taboption("AdGuardHomeconf",Value,"AdGuardHomeconf",nil,translate("开头的数字符号（＃）或分号的每一行（;）被视为注释；删除（;）启用指定选项。"))
-conf.template="cbi/tvalue"
-conf.rows=20
-conf.wrap="off"
-conf.cfgvalue=function(t,t)
-return e.readfile("/etc/config/AdGuardHome")or""
-end
-conf.write=function(a,a,t)
-if t then
-t=t:gsub("\r\n?","\n")
-e.writefile("/tmp/AdGuardHome",t)
-if(luci.sys.call("cmp -s /tmp/AdGuardHome /etc/config/AdGuardHome")==1)then
-e.writefile("/etc/config/AdGuardHome",t)
-luci.sys.call("/etc/init.d/AdGuardHome restart >/dev/null")
-end
-e.remove("/tmp/AdGuardHome")
-end
-end
-end
 if nixio.fs.access("/etc/pcap-dnsproxy/Config.conf")then
-s:tab("pcapconf",translate("配置pcap-dnsproxy"),translate("本页是配置/etc/pcap-dnsproxy/Config.conf的文档内容。应用保存后自动重启生效"))
+s:tab("pcapconf",translate("配置pcap-dnsproxy"),translate("本页是配置/etc/pcap-dnsproxy/Config.conf的文档内容。应用保存后自动重启生效！"))
 conf=s:taboption("pcapconf",Value,"pcapconf",nil,translate("开头的数字符号（＃）或分号的每一行（;）被视为注释；删除（;）启用指定选项。"))
 conf.template="cbi/tvalue"
 conf.rows=20
@@ -242,7 +242,7 @@ end
 end
 end
 if nixio.fs.access("/etc/wifidog.conf")then
-s:tab("wifidogconf",translate("配置wifidog"),translate("本页是配置/etc/wifidog.conf的文档内容。应用保存后自动重启生效"))
+s:tab("wifidogconf",translate("配置wifidog"),translate("本页是配置/etc/wifidog.conf的文档内容。应用保存后自动重启生效！"))
 conf=s:taboption("wifidogconf",Value,"wifidogconf",nil,translate("开头的数字符号（＃）或分号的每一行（;）被视为注释；删除（;）启用指定选项。"))
 conf.template="cbi/tvalue"
 conf.rows=20
