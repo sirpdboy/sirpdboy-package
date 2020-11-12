95fead8bb8665f757ceac6cca42ac3b80c8cfdf1
sirpdboy <sirpdboy@qq.com>
2020-11-13 01:52:09 +0800

    up theme argon_new

--- luci-app-ddnsto/luasrc/controller/ddnsto.lua
+++ luci-app-ddnsto/luasrc/controller/ddnsto.lua
@@ -5,10 +5,11 @@ return
 end
 entry({"admin","services","ddnsto"},cbi("ddnsto/global"),_("ddnsto"),57).dependent=true
 entry({"admin","services","ddnsto","status"},call("act_status")).leaf=true
+
 end
 function act_status()
 local e={}
-e.ddnsto=luci.sys.call("pgrep /usr/bin/ddnsto >/dev/null")==0
+e.ddnsto=luci.sys.call("pidof ddnsto >/dev/null")==0
 luci.http.prepare_content("application/json")
 luci.http.write_json(e)
 end
