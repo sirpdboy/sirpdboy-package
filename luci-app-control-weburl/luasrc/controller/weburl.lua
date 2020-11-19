0393bee1d91860880cbae4144457a432167b71fe
sirpdboy <sirpdboy@qq.com>
2020-11-20 01:04:00 +0800

    upset

--- luci-app-control-weburl/luasrc/controller/weburl.lua
+++ luci-app-control-weburl/luasrc/controller/weburl.lua
@@ -4,7 +4,7 @@ function index()
     if not nixio.fs.access("/etc/config/weburl") then return end
 
     entry({"admin", "control"}, firstchild(), "Control", 44).dependent = false
-    entry({"admin", "control", "weburl"}, cbi("weburl"), _("网址过滤刀"), 12).dependent =
+    entry({"admin", "control", "weburl"}, cbi("weburl"), _("网址过滤"), 12).dependent =
         true
 end
 
