31300e9af74550f4031e852160305b822d3b1a9e
sirpdboy <sirpdboy@qq.com>
2020-11-22 00:28:50 +0800

    Up weburl

--- luci-app-control-weburl/luasrc/controller/weburl.lua
+++ luci-app-control-weburl/luasrc/controller/weburl.lua
@@ -4,7 +4,7 @@ function index()
     if not nixio.fs.access("/etc/config/weburl") then return end
 
     entry({"admin", "control"}, firstchild(), "Control", 44).dependent = false
-    entry({"admin", "control", "weburl"}, cbi("weburl"), _("网址过滤"), 12).dependent =
+    entry({"admin", "control", "weburl"}, cbi("weburl"), _("管控过滤"), 1).dependent =
         true
 end
 
