7aaceadbcd0973a67323838ff3f2b323d977177a
sirpdboy <sirpdboy@qq.com>
2021-02-26 00:17:41 +0800

    add ddnsto

--- luci-app-ddnsto/luasrc/controller/ddnsto.lua
+++ luci-app-ddnsto/luasrc/controller/ddnsto.lua
@@ -1,14 +1,22 @@
-module("luci.controller.ddnsto",package.seeall)
+module("luci.controller.ddnsto", package.seeall)
+
 function index()
-if not nixio.fs.access("/etc/config/ddnsto")then
-return
+	if not nixio.fs.access("/etc/config/ddnsto") then
+		return
+	end
+
+	entry({"admin", "services", "ddnsto"}, cbi("ddnsto"), _("DDNS.to"), 57).dependent = true
+
+	entry({"admin", "services", "ddnsto_status"}, call("ddnsto_status"))
 end
-entry({"admin","services","ddnsto"},cbi("ddnsto/global"),_("ddnsto"),57).dependent=true
-entry({"admin","services","ddnsto","status"},call("act_status")).leaf=true
-end
-function act_status()
-local e={}
-e.ddnsto=luci.sys.call("pgrep /usr/bin/ddnsto >/dev/null")==0
-luci.http.prepare_content("application/json")
-luci.http.write_json(e)
+
+function ddnsto_status()
+	local sys  = require "luci.sys"
+
+	local status = {
+		running = (sys.call("pidof ddnsto >/dev/null") == 0)
+	}
+
+	luci.http.prepare_content("application/json")
+	luci.http.write_json(status)
 end
