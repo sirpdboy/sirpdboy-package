acb52bca33e49c49c9a724d01c025fcc4805866f
sirpdboy <sirpdboy@qq.com>
2021-04-06 09:47:23 +0800

    up dnsto

--- luci-app-dnsto/luasrc/model/cbi/dnsto/global.lua
+++ luci-app-dnsto/luasrc/model/cbi/dnsto/global.lua
@@ -1,12 +1,12 @@
 local e=require"nixio.fs"
 local e=luci.http
 local a,t,e
-a=Map("dns-to",translate("dns-to"),translate("ddnsto是支持http2的快速远程路由工具。</br>你需要先到 https://www.ddnsto.com/app/#/weixinlogin注册，然后在本插件内填入Token，再登录 https://www.ddnsto.com/app/#/weixinlogin设置穿透。"))
-a.template="dns-to/index"
+a=Map("dnsto",translate("dnsto"),translate("ddnsto是支持http2的快速远程路由工具。</br>你需要先到 https://www.ddnsto.com/app/#/weixinlogin注册，然后在本插件内填入Token，再登录 https://www.ddnsto.com/app/#/weixinlogin设置穿透。"))
+a.template="dnsto/index"
 t=a:section(TypedSection,"global",translate("Running Status"))
 t.anonymous=true
 e=t:option(DummyValue,"_status",translate("Running Status"))
-e.template="dns-to/dvalue"
+e.template="dnsto/dvalue"
 e.value=translate("Collecting data...")
 t=a:section(TypedSection,"global",translate("全局设置"),translate("设置教程:</font><a style=\"color: #ff0000;\" onclick=\"window.open('https://www.ddnsto.com/zh/guide/koolshare_merlin.html')\">点击跳转官网教程</a>"))
 t.anonymous=true
@@ -21,7 +21,7 @@ e.rmempty=true
 e=t:option(Value,"token",translate('ddnsto 令牌'))
 e.password=true
 e.rmempty=false
-if nixio.fs.access("/etc/config/dns-to")then
+if nixio.fs.access("/etc/config/dnsto")then
 e=t:option(Button,"Configuration",translate("域名配置管理"))
 e.inputtitle=translate("打开网站")
 e.inputstyle="reload"
