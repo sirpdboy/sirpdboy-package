393357280ed2997f62375e8ad60ef89d57a500f2
sirpdboy <sirpdboy@qq.com>
2021-04-15 15:19:13 +0800

    up dnsto 414

--- luci-app-dnsto/luasrc/controller/dnsto.lua
+++ luci-app-dnsto/luasrc/controller/dnsto.lua
@@ -9,7 +9,7 @@ end
 
 function act_status()
   local e={}
-  e.running=luci.sys.call("pgrep -f dnsto >/dev/null")==0
+  e.running=luci.sys.call("pgrep -f ddnsto >/dev/null")==0
   luci.http.prepare_content("application/json")
   luci.http.write_json(e)
 
