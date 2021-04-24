9af2beb86da40283eb7e73344406b50ebd2d049b
sirpdboy <sirpdboy@qq.com>
2021-04-24 15:37:06 +0800

    luci-app-wrtbwmon1.6.3

---
+++ luci-app-wrtbwmon/luasrc/controller/wrtbwmon.lua
@@ -0,0 +1,10 @@
+module("luci.controller.wrtbwmon", package.seeall)
+
+function index()
+	
+entry({"admin", "nlbw", "usage"},alias("admin", "nlbw", "usage", "details"),_("Usage"), 1)
+entry({"admin", "nlbw", "usage", "details"},	template("wrtbwmon"),_("Details"), 10).leaf=true
+entry({"admin", "nlbw", "usage", "config"},arcombine(cbi("wrtbwmon/config")),_("Configuration"), 20).leaf=true
+entry({"admin", "nlbw", "usage", "custom"},form("wrtbwmon/custom"),_("User file"), 30).leaf=true
+end
+
