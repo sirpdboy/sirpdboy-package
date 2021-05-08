4f5a44c4670a7b201e15c2b4c3ef822714bea939
sirpdboy <sirpdboy@qq.com>
2021-05-08 14:15:48 +0800

    cpulimit

--- luci-app-cpulimit/root/usr/bin/cpulimit.sh
+++ luci-app-cpulimit/root/usr/bin/cpulimit.sh
@@ -8,8 +8,9 @@ cpulimit_get(){
 	[ $enabled -gt 0 ] || return 1
 	config_get limit $1 limit
 	config_get exename $1 exename
-	cpulimit -l $limit -e $exename &
-	echo cpulimit -l $limit -e $exename
+	eval exepid=$(pgrep $exename)
+	cpulimit -l $limit -p $exepid &
+	echo cpulimit -l $limit -p $exepid
 }
 
 
