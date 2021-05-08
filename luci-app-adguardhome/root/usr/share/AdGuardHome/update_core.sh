144ce4e00c4d1823b9efa93ebccad367faddf48e
sirpdboy <sirpdboy@qq.com>
2021-05-08 14:40:48 +0800

    adguardhome

--- luci-app-adguardhome/root/usr/share/AdGuardHome/update_core.sh
+++ luci-app-adguardhome/root/usr/share/AdGuardHome/update_core.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 PATH="/usr/sbin:/usr/bin:/sbin:/bin"
 binpath=$(uci get AdGuardHome.AdGuardHome.binpath)
 if [ -z "$binpath" ]; then
@@ -15,7 +15,7 @@ check_if_already_running(){
 
 check_wgetcurl(){
 	which curl && downloader="curl -L -k --retry 2 --connect-timeout 20 -o" && return
-	which wget && downloader="wget --no-check-certificate -t 2 -T 20 -O" && return
+	which wget-ssl && downloader="wget-ssl --no-check-certificate -t 2 -T 20 -O" && return
 	[ -z "$1" ] && opkg update || (echo error opkg && EXIT 1)
 	[ -z "$1" ] && (opkg remove wget wget-nossl --force-depends ; opkg install wget ; check_wgetcurl 1 ;return)
 	[ "$1" == "1" ] && (opkg install curl ; check_wgetcurl 2 ; return)
@@ -23,11 +23,12 @@ check_wgetcurl(){
 }
 check_latest_version(){
 	check_wgetcurl
-	latest_ver="$($downloader - https://api.github.com/repos/AdguardTeam/AdGuardHome/releases/latest 2>/dev/null|grep -E 'tag_name' |grep -E 'v[0-9.]+' -o 2>/dev/null)"
+	control_ver=`uci get AdGuardHome.AdGuardHome.cversion 2>/dev/null`
+	[ "$control_ver" = "0" -o -z "$control_ver" ] && latest_ver="$($downloader - https://api.github.com/repos/AdguardTeam/AdGuardHome/releases/latest 2>/dev/null|grep -E 'tag_name' |grep -E 'v[0-9.]+' -o 2>/dev/null)" || latest_ver="$control_ver"
 	if [ -z "${latest_ver}" ]; then
 		echo -e "\nFailed to check latest version, please try again later."  && EXIT 1
 	fi
-	now_ver="$($binpath -c /dev/null --check-config 2>&1| grep -m 1 -E 'v[0-9.]+' -o)"
+	now_ver="$($binpath --version 2>/dev/null | grep -m 1 -E '[0-9]+[.][0-9.]+' -o)"
 	if [ "${latest_ver}"x != "${now_ver}"x ] || [ "$1" == "force" ]; then
 		echo -e "Local version: ${now_ver}., cloud version: ${latest_ver}." 
 		doupdate_core
@@ -130,20 +131,16 @@ doupdate_core(){
 	Arch="amd64"
 	;;
 	"mipsel")
-	Arch="mipsle"
+	Arch="mipsle_softfloat"
 	;;
 	"mips64el")
-	Arch="mips64le"
-	Arch="mipsle"
-	echo -e "mips64el use $Arch may have bug" 
+	Arch="mips64le_softfloat"
 	;;
 	"mips")
-	Arch="mips"
+	Arch="mips_softfloat"
 	;;
 	"mips64")
-	Arch="mips64"
-	Arch="mips"
-	echo -e "mips64 use $Arch may have bug" 
+	Arch="mips64_softfloat"
 	;;
 	"arm")
 	Arch="arm"
