59e6de0caa68084155b0abc438d560313659e3fe
sirpdboy <sirpdboy@qq.com>
2021-05-11 14:56:07 +0800

    timectrll

--- luci-app-timecontrol/luasrc/controller/timecontrol.lua
+++ luci-app-timecontrol/luasrc/controller/timecontrol.lua
@@ -4,7 +4,7 @@ function index()
     if not nixio.fs.access("/etc/config/timecontrol") then return end
 
     entry({"admin", "control"}, firstchild(), "Control", 44).dependent = false
-    entry({"admin", "control", "timecontrol"}, cbi("timecontrol"), _("Internet Time Control"), 10).dependent =
+    entry({"admin", "control", "timecontrol"}, cbi("timecontrol"), _("Ê±¼ä¿ØÖÆ"), 10).dependent =
         true
     entry({"admin", "control", "timecontrol", "status"}, call("status")).leaf = true
 end
