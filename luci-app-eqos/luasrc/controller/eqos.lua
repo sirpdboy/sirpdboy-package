9317736c5354eb226a3a8523852534e2faeee7d2
sirpdboy <sirpdboy@qq.com>
2021-05-11 15:24:42 +0800

    eqos

--- luci-app-eqos/luasrc/controller/eqos.lua
+++ luci-app-eqos/luasrc/controller/eqos.lua
@@ -7,6 +7,6 @@ function index()
 	
 	local page
 
-	page = entry({"admin", "network", "eqos"}, cbi("eqos"), "EQoS", 90)
+	page = entry({"admin", "control", "eqos"}, cbi("eqos"), "IP限速", 90)
 	page.dependent = true
 end
