d4377025c15b59e2bc9c7085f33a37fadbfce0be
sirpdboy <sirpdboy@qq.com>
2021-06-03 12:23:14 +0800

    up netdata1.30.1

--- luci-app-netdata/luasrc/controller/netdata.lua
+++ luci-app-netdata/luasrc/controller/netdata.lua
@@ -6,7 +6,7 @@ function index()
 	end
 	local fs = require "nixio.fs"
 
-	entry({"admin","status","netdata"},template("netdata"),_("实时监控"),10).leaf=true
+	entry({"admin","status","netdata"},template("netdata"),_("NetData"),10).leaf=true
 
 
 end
