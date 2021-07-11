891a213a898eb01b94d7b6784392071c9977e88b
sirpdboy <sirpdboy@qq.com>
2021-07-11 23:53:33 +0800

    up advanced up unc

--- luci-app-advanced/luasrc/model/cbi/advanced.lua
+++ luci-app-advanced/luasrc/model/cbi/advanced.lua
@@ -1,9 +1,39 @@
 local e=require"nixio.fs"
 local t=require"luci.sys"
+local t=luci.model.uci.cursor()
 m=Map("advanced",translate("高级进阶设置"),translate("<font color=\"Red\"><strong>配置文档是直接编辑的除非你知道自己在干什么，否则请不要轻易修改这些配置文档。配置不正确可能会导致不能开机等错误。</strong></font><br/>"))
 m.apply_on_parse=true
 s=m:section(TypedSection,"advanced")
 s.anonymous=true
+s:tab("base",translate("Basic Settings"))
+o=s:taboption("base",Flag,"usb3_disable",translate("关闭USB3.0"),translate("勾选以关闭USB3.0，降低2.4G无线干扰。"))
+o.default=0
+
+o=s:taboption("base",ListValue,"lan2wan",translate("LAN改WAN"),translate("选择将其中一个LAN口改设为WAN口，以使用多线接入。"))
+o:value("none",translate("当前模式"))
+o:value("1",translate("LAN1"))
+o:value("0",translate("LAN2"))
+o:value("2",translate("LAN3"))
+o:value("factory",translate("默认状态"))
+o.default="none"
+
+rollbacktime=t:get("luci","apply","rollback")
+o=s:taboption("base",Value,"rollback",translate("超时时间"),translate("设置LUCI超时回滚时间，默认30秒。"))
+o.datatypes="and(uinteger,min(20))"
+o.default=rollbacktime
+
+o=s:taboption("base",ListValue,"webshell",translate("WebShell"),translate("选择要使用的WebShell服务"))
+o:value("ttyd",translate("ttyd"))
+o:value("shellinabox",translate("shellinabox"))
+o.default="ttyd"
+
+o=s:taboption("base",ListValue,"route_mode",translate("运行模式"),translate("AP模式：请通过WAN网口连接，AP自身管理地址由上级路由DHCP分配，如需固定请修改LAN口地址。<br>并闭AP模式请选回“路由模式”。两种模式均为一次性动作，切换完成之后运行模式将自动显示为“当前模式。"))
+o.default="none"
+o:value("none",translate("当前模式"))
+o:value("apmode",translate("AP模式"))
+o:value("dhcpmode",translate("路由模式"))
+
+
 if nixio.fs.access("/etc/dnsmasq.conf")then
 
 s:tab("dnsmasqconf",translate("dnsmasq"),translate("本页是配置/etc/dnsmasq.conf的文档内容。应用保存后自动重启生效"))
@@ -282,4 +312,26 @@ e.remove("/tmp/openclash")
 end
 end
 end
+
+if nixio.fs.access("/bin/nuc")then
+	s:tab("mode",translate("模式切换(适用软路由）"),translate("<br />可以在这里切换NUC和正常模式，重置你的网络设置。<br /><font color=\"Red\"><strong>点击后会立即重启设备，没有确认过程，请谨慎操作！</strong></font><br/>"))
+	o=s:taboption("mode",Button,"nucmode",translate("切换为NUC模式"),translate("<font color=\"green\"><strong>本模式适合于单网口主机，如NUC、单网口电脑，需要配合VLAN交换机使用！<br />默认gateway是：192.168.2.1，ipaddr是192.168.2.2。用本机接口LAN接上级LAN当NAS。</strong></font><br/>"))
+	o.inputtitle=translate("NUC模式")
+	o.inputstyle="reload"
+
+	o.write=function()
+	luci.sys.call("/bin/nuc")
+	end
+
+
+
+	o=s:taboption("mode",Button,"normalmode",translate("切换成正常模式"),translate("<font color=\"green\"><strong>本模式适合于有两个网口或以上的设备使用，如多网口软路由或者虚拟了两个以上网口的虚拟机使用！</strong></font><br/>"))
+	o.inputtitle=translate("正常模式")
+	o.inputstyle="reload"
+
+	o.write=function()
+	luci.sys.call("/bin/normalmode")
+	end
+end
+
 return m
