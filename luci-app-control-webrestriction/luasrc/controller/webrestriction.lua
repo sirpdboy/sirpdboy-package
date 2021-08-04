d62d0ff6ba0400d2cb9005d5e00caf2f4941b61b
sirpdboy <sirpdboy@qq.com>
2021-08-04 21:34:26 +0800

    remove md

--- luci-app-control-webrestriction/luasrc/controller/webrestriction.lua
+++
@@ -1,9 +0,0 @@
-module("luci.controller.webrestriction", package.seeall)
-
-function index()
-    if not nixio.fs.access("/etc/config/webrestriction") then return end
-
-    entry({"admin", "control"}, firstchild(), "Control", 44).dependent = false
-    entry({"admin", "control", "webrestriction"}, cbi("webrestriction"), _("访问限制"), 11).dependent = true
- end
-
