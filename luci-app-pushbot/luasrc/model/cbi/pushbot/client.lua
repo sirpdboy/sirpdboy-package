2e9a8c2d8b92e9950e5fbf78e2393c11e33090a8
SirPdboy <63764630+sirpdboy@users.noreply.github.com>
2021-09-09 00:34:22 +0800

    Delete luci-app-pushbot directory

--- luci-app-pushbot/luasrc/model/cbi/pushbot/client.lua
+++
@@ -1,6 +0,0 @@
-f = SimpleForm("pushbot")
-luci.sys.call("/usr/bin/pushbot/pushbot client")
-f.reset = false
-f.submit = false
-f:append(Template("pushbot/client"))
-return f
--- luci-app-pushbot/luasrc/model/cbi/pushbot/log.lua
+++
@@ -1,5 +0,0 @@
-f = SimpleForm("pushbot")
-f.reset = false
-f.submit = false
-f:append(Template("pushbot/log"))
-return f
