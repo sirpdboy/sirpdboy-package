b7faa5b962abd29901201cc13cf0d795770ed6fe
SirPdboy <63764630+sirpdboy@users.noreply.github.com>
2021-10-09 09:44:56 +0800

    Delete luci-app-chinadns-ng directory

--- luci-app-chinadns-ng/luasrc/controller/chinadns-ng.lua
+++
@@ -1,8 +0,0 @@
-module("luci.controller.chinadns-ng", package.seeall)
-
-function index()
-	if not nixio.fs.access("/etc/config/chinadns-ng") then
-		return
-	end
-	entry({"admin", "dns", "chinadns-ng"}, cbi("chinadns-ng"), _("ChinaDNS-NG"), 70).dependent = true
-end
