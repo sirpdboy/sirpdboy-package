80442c286236893b0f7f69e5df4762da4bd02a8e
SirPdboy <63764630+sirpdboy@users.noreply.github.com>
2022-05-14 21:43:47 +0800

    up adguardhome107.6

--- luci-app-adguardhome/luasrc/controller/AdGuardHome.lua
+++ luci-app-adguardhome/luasrc/controller/AdGuardHome.lua
@@ -21,31 +21,29 @@ end
 function get_template_config()
 	local b
 	local d=""
-	local file = "/tmp/resolv.conf.d/resolv.conf.auto"
-	if not fs.access(file) then
-		file = "/tmp/resolv.conf.auto"
+	for cnt in io.lines("/tmp/resolv.conf.d/resolv.conf.auto") do
+		b=string.match (cnt,"^[^#]*nameserver%s+([^%s]+)$")
+		if (b~=nil) then
+			d=d.."  - "..b.."\n"
+		end
 	end
-    for cnt in io.lines(file) do
-        b = string.match(cnt, "^[^#]*nameserver%s+([^%s]+)$")
-        if (b ~= nil) then d = d .. "  - " .. b .. "\n" end
-    end
-    local f = io.open("/usr/share/AdGuardHome/AdGuardHome_template.yaml", "r+")
-    local tbl = {}
-    local a = ""
-    while (1) do
-        a = f:read("*l")
-        if (a == "#bootstrap_dns") then
-            a = d
-        elseif (a == "#upstream_dns") then
-            a = d
-        elseif (a == nil) then
-            break
-        end
-        table.insert(tbl, a)
-    end
-    f:close()
-    http.prepare_content("text/plain; charset=utf-8")
-    http.write(table.concat(tbl, "\n"))
+	local f=io.open("/usr/share/AdGuardHome/AdGuardHome_template.yaml", "r+")
+	local tbl = {}
+	local a=""
+	while (1) do
+    	a=f:read("*l")
+		if (a=="#bootstrap_dns") then
+			a=d
+		elseif (a=="#upstream_dns") then
+			a=d
+		elseif (a==nil) then
+			break
+		end
+		table.insert(tbl, a)
+	end
+	f:close()
+	http.prepare_content("text/plain; charset=utf-8")
+	http.write(table.concat(tbl, "\n"))
 end
 function reload_config()
 	fs.remove("/tmp/AdGuardHometmpconfig.yaml")
