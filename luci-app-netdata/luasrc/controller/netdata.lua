4c992cc176b029ce6cb95d37a07ba4cab559e8b0
SirPdboy <63764630+sirpdboy@users.noreply.github.com>
2022-07-20 02:30:12 +0800

    增加重启服务

--- luci-app-netdata/luasrc/controller/netdata.lua
+++ luci-app-netdata/luasrc/controller/netdata.lua
@@ -3,4 +3,10 @@ module("luci.controller.netdata", package.seeall)
 function index()
 
 	entry({"admin", "status", "netdata"}, template("netdata"), _("NetData"), 10).leaf = true
+	entry({"admin", "status", "netdata", "reinit"}, call("act_reinit"))
+end
+
+
+function act_reinit()
+	luci.sys.exec('/etc/init.d/netdate stop   >/dev/null  && /etc/init.d/netdate start  >/dev/null ') 
 end
