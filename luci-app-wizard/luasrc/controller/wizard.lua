7e50e4053ab1b3ff1f302551e1aa0fcc94083e79
SirPdboy <63764630+sirpdboy@users.noreply.github.com>
2022-11-28 03:08:08 +0800

    up luci-app-wizard

---
+++ luci-app-wizard/luasrc/controller/wizard.lua
@@ -0,0 +1,14 @@
+-- Copyright 2019 X-WRT <dev@x-wrt.com>
+-- Copyright 2022 sirpdboy
+
+module("luci.controller.wizard", package.seeall)
+function index()
+	if not nixio.fs.access("/etc/config/wizard") then
+		return
+	end
+		local page 
+		page = entry({"admin", "Statistics"}, cbi("wizard/wizard"), _("Inital Setup"), 21)
+		page.i18n = "wizard"
+		page.dependent = true
+
+end
