8956caed4ec4b70454094071ec0f582335d8b9b3
SirPdboy <63764630+sirpdboy@users.noreply.github.com>
2023-02-24 14:11:39 +0800

    u

--- upnpd/luci-app-upnp/luasrc/model/cbi/upnp/upnp.lua
+++ upnpd/luci-app-upnp/luasrc/model/cbi/upnp/upnp.lua
@@ -1,6 +1,8 @@
 -- Copyright 2008 Steven Barth <steven@midlink.org>
 -- Copyright 2008-2011 Jo-Philipp Wich <jow@openwrt.org>
 -- Licensed to the public under the Apache License 2.0.
+local uci = require 'luci.model.uci'.cursor()
+local gateip = 'http://' .. uci:get_first('network', 'lan', 'ipaddr')
 require("luci.tools.webadmin")
 m = Map("upnpd", luci.util.pcdata(translate("Universal Plug & Play")),
 	translate("UPnP allows clients in the local network to automatically configure the router."))
@@ -43,7 +45,7 @@ s:taboption("general", Flag, "igdv1", translate("Enable IGDv1 mode"),
 	translate("Advertise as IGDv1 device instead of IGDv2")).default = "1"
 
 pu = s:taboption("general", Value, "presentation_url", translate("Presentation URL"))
-pu.placeholder = "http://192.168.10.1/"
+pu..default=gateip
 
 s:taboption("general", Flag, "log_output", translate("Enable additional logging"),
 	translate("Puts extra debugging information into the system log"))
