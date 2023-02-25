9bf872f06d7af26291acc702f5d4a975472b2f5c
SirPdboy <63764630+sirpdboy@users.noreply.github.com>
2023-02-25 15:07:22 +0800

    b

--- luci-app-adguardhome/luasrc/model/cbi/AdGuardHome/base.lua
+++ luci-app-adguardhome/luasrc/model/cbi/AdGuardHome/base.lua
@@ -24,7 +24,7 @@ o.placeholder=3000
 o.default=3000
 o.datatype="port"
 o.optional = false
-o.description = translate("<input type='button' style='width:210px; border-color:Teal; text-align:center; font-weight:bold;color:Green;background:#f36c21;' value='AdGuardHome Web:" .. httpport .. "' onclick=\"window.open('http://'+window.location.hostname+':" .. httpport .. "')\"/>")
+o.description = translate("<input type='button' style='width:210px; border-color:Teal; text-align:center;padding: .75rem 1rem; font-weight:bold;color:Green;background:#f36c21;' value='AdGuardHome Web:" .. httpport .. "' onclick=\"window.open('http://'+window.location.hostname+':" .. httpport .. "')\"/>")
 ---- update warning not safe
 local binmtime=uci:get("AdGuardHome","AdGuardHome","binmtime") or "0"
 local e=""
