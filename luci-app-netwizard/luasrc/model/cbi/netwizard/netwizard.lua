47226c305f61bed014f02d2e57b54e502c80153b
SirPdboy <63764630+sirpdboy@users.noreply.github.com>
2023-05-02 21:37:59 +0800

    up netwizard 1.7 解决防火墙等问题。功能更强大的网络设置向导。

--- luci-app-netwizard/luasrc/model/cbi/netwizard/netwizard.lua
+++ luci-app-netwizard/luasrc/model/cbi/netwizard/netwizard.lua
@@ -1,13 +1,22 @@
 -- Copyright 2019 X-WRT <dev@x-wrt.com>
 -- Copyright 2022 sirpdboy
-
-local o = require "luci.sys"
+local sdns = {
+   "223.5.5.5",
+   "180.76.76.76", 
+   "114.114.114.114",
+   "8.8.8.8",
+   "1.1.1.1"
+}
 local net = require "luci.model.network".init()
 local sys = require "luci.sys"
 local ifaces = sys.net:devices()
 local nt = require "luci.sys".net
 local uci = require("luci.model.uci").cursor()
-
+local lan_gateway = uci:get("netwizard", "default", "lan_gateway")
+if lan_gateway ~= "" then
+   lan_gateway = sys.exec("ipaddr=`uci -q get network.lan.ipaddr`;echo ${ipaddr%.*}")
+end
+local validation = require "luci.cbi.datatypes"
 local has_wifi = false
 uci:foreach("wireless", "wifi-device",
 		function(s)
@@ -15,7 +24,7 @@ uci:foreach("wireless", "wifi-device",
 			return false
 		end)
 
-local m = Map("netwizard", luci.util.pcdata(translate("Inital Router Setup")), translate("Quick network setup wizard. If you need more settings, please enter network - interface to set.</br>")..translate("The network card is automatically set, and the physical interfaces other than the specified WAN interface are automatically bound as LAN ports, and all side routes are bound as LAN ports.</br>")..translate("For specific usage, see:")..translate("<a href=\'https://github.com/sirpdboy/luci-app-wizard.git' target=\'_blank\'>GitHub @sirpdboy/luci-app-wizard</a>") )
+local m = Map("netwizard", luci.util.pcdata(translate("Inital Router Setup")), translate("Quick network setup wizard. If you need more settings, please enter network - interface to set.</br>")..translate("The network card is automatically set, and the physical interfaces other than the specified WAN interface are automatically bound as LAN ports, and all side routes are bound as LAN ports.</br>")..translate("For specific usage, see:")..translate("<a href=\'https://github.com/sirpdboy/luci-app-netwizard.git' target=\'_blank\'>GitHub @sirpdboy/netwizard</a>") )
 
 local s = m:section(TypedSection, "netwizard", "")
 s.addremove = false
@@ -26,11 +35,8 @@ if has_wifi then
 	s:tab("wifisetup", translate("Wireless Settings"), translate("Set the router's wireless name and password. For more advanced settings, please go to the Network-Wireless page."))
 end
 s:tab("othersetup", translate("Other Settings"))
-
-local lan_zipaddr = uci:get("network", "lan", "ipaddr")
 local e = s:taboption("wansetup", Value, "lan_ipaddr", translate("Lan IPv4 address") ,translate("You must specify the IP address of this machine, which is the IP address of the web access route"))
-e:value(lan_zipaddr, translate(lan_zipaddr .. "(The IP of the current LAN)"))
-e.default = lan_zipaddr
+e.default = "" .. uci:get("network", "lan", "ipaddr")
 e.datatype = "ip4addr"
 
 e = s:taboption("wansetup", Value, "lan_netmask", translate("Lan IPv4 netmask"))
@@ -38,6 +44,7 @@ e.datatype = "ip4addr"
 e:value("255.255.255.0")
 e:value("255.255.0.0")
 e:value("255.0.0.0")
+e.default = '255.255.255.0'
 
 e = s:taboption("wansetup", ListValue, "ipv6",translate('Select IPv6 mode'),translate("Caution: If you delete IPV6 related plug-ins completely, you will not be able to recover"))
 e:value('0', translate('Disable IPv6'))
@@ -88,47 +95,60 @@ e.datatype = "ip4addr"
 e:value("255.255.255.0")
 e:value("255.255.0.0")
 e:value("255.0.0.0")
+e.default = '255.255.255.0'
 
 e = s:taboption("wansetup", Value, "wan_gateway", translate("Wan IPv4 gateway"))
 e:depends({wan_proto="static"})
 e.datatype = "ip4addr"
 
-
-e = s:taboption("wansetup", Value, "wan_dns", translate("Use custom Wan DNS"))
+e = s:taboption("wansetup", DynamicList, "wan_dns", translate("Use custom Wan DNS"))
 e:value("223.5.5.5", translate("Ali DNS:223.5.5.5"))
 e:value("180.76.76.76", translate("Baidu dns:180.76.76.76"))
 e:value("114.114.114.114", translate("114 DNS:114.114.114.114"))
-e.default = "223.5.5.5"
-e:depends({wan_proto="dhcp"})
+e:value("8.8.8.8", translate("Google DNS:8.8.8.8"))
+e:value("1.1.1.1", translate("Cloudflare DNS:1.1.1.1"))
+-- e.default = "223.5.5.5"
 e:depends({wan_proto="static"})
 e:depends({wan_proto="pppoe"})
 e.datatype = "ip4addr"
 e.cast = "string"
 
 e = s:taboption("wansetup", Value, "lan_gateway", translate("Lan IPv4 gateway"), translate( "Please enter the main routing IP address. The bypass gateway is not the same as the login IP of this bypass WEB and is in the same network segment"))
-e:depends({wan_proto="siderouter"})
+e.default = lan_gateway
+e:depends({wan_proto = "siderouter"})
 e.datatype = "ip4addr"
 
-e = s:taboption("wansetup", Value, "lan_dns", translate("Use custom Siderouter DNS"))
+e = s:taboption("wansetup", DynamicList, "lan_dns", translate("Use custom Siderouter DNS"))
 e:value("223.5.5.5", translate("Ali DNS:223.5.5.5"))
 e:value("180.76.76.76", translate("Baidu dns:180.76.76.76"))
 e:value("114.114.114.114", translate("114 DNS:114.114.114.114"))
+e:value("8.8.8.8", translate("Google DNS:8.8.8.8"))
+e:value("1.1.1.1", translate("Cloudflare DNS:1.1.1.1"))
 e.default = "223.5.5.5"
 e:depends({wan_proto="siderouter"})
 e.datatype = "ip4addr"
 e.cast = "string"
 
-e = s:taboption("wansetup", Flag, "synflood", translate("Enable SYN-flood defense"))
-e:depends({wan_proto="siderouter"})
-e.default = true
+synflood = s:taboption("wansetup", Flag, "synflood", translate("Enable SYN-flood defense"))
+synflood.default = 1
+synflood.anonymous = false
 
-e = s:taboption("wansetup", Flag, "masq", translate("Enable IP dynamic camouflage"),translate("Enable IP dynamic camouflage when the side routing network is not ideal"))
-e:depends({wan_proto="siderouter"})
-e.default = true
+redirectdns = s:taboption("wansetup", Flag, "redirectdns", translate("Custom Firewall Force DNS Forwarding"),translate("Use iptables to force all TCP/UDP DNS 53ports in IPV4/IPV6 to be forwarded from this route"))
+redirectdns:depends({wan_proto="dhcp"})
+redirectdns:depends({wan_proto="static"})
+redirectdns:depends({wan_proto="pppoe"})
+redirectdns.default = 1
+redirectdns.anonymous = false
 
-e = s:taboption("wansetup", Flag, "lan_snat", translate("Firewall regulations"),translate("Bypass firewall settings, when Xiaomi or Huawei are used as the main router, the WIFI signal cannot be used normally"))
-e:depends({wan_proto="siderouter"})
-e.default = true
+masq = s:taboption("wansetup", Flag, "masq", translate("Enable IP dynamic camouflage"),translate("Enable IP dynamic camouflage when the side routing network is not ideal"))
+masq:depends({wan_proto="siderouter"})
+masq.default = 1
+masq.anonymous = false
+
+lan_snat = s:taboption("wansetup", Flag, "lan_snat", translate("Custom firewall"),translate("Bypass firewall settings, when Xiaomi or Huawei are used as the main router, the WIFI signal cannot be used normally"))
+lan_snat:depends({wan_proto="siderouter"})
+lan_snat.default = 1
+lan_snat.anonymous = false
 
 e = s:taboption("wansetup", Value, "snat_tables", translate(" "))
 e:value("iptables -t nat -I POSTROUTING -o br-lan -j MASQUERADE")
@@ -137,7 +157,9 @@ e.default = "iptables -t nat -I POSTROUTING -o br-lan -j MASQUERADE"
 e.anonymous = false
 e:depends("lan_snat", true)
 
-e = s:taboption("wansetup", Flag, "lan_dhcp", translate("Enable DHCP Server"), translate("If not selected, DHCP is disabled by default. If DHCP is used, the main route DHCP needs to be turned off. To disable DHCP, you need to manually change all Internet device gateways and DNS to this routing IP"))
+lan_dhcp = s:taboption("wansetup", Flag, "lan_dhcp", translate("Enable DHCP Server"), translate("If not selected, DHCP is disabled by default. If DHCP is used, the main route DHCP needs to be turned off. To disable DHCP, you need to manually change all Internet device gateways and DNS to this routing IP"))
+lan_dhcp.default = 1
+lan_dhcp.anonymous = false
 
 e = s:taboption("wansetup", Flag, "dnsset", translate("Enable DNS notifications (ipv4/ipv6)"),translate("Force the DNS server in the DHCP server to be specified as the IP for this route"))
 e:depends("lan_dhcp", true)
@@ -145,7 +167,6 @@ e:depends("lan_dhcp", true)
 if has_wifi then
 	e = s:taboption("wifisetup", Value, "wifi_ssid", translate("<abbr title=\"Extended Service Set Identifier\">ESSID</abbr>"))
 	e.datatype = "maxlength(32)"
-
 	e = s:taboption("wifisetup", Value, "wifi_key", translate("Key"))
 	e.datatype = "wpakey"
 	e.password = true
