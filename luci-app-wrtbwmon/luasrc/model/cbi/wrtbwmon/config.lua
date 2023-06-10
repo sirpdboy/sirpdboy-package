621fecd0c102f809d7bbe6341ded6d0f7d3252a6
SirPdboy <63764630+sirpdboy@users.noreply.github.com>
2023-06-10 10:11:10 +0800

    up wrtbwmon 上行 带宽和下行带宽分开设置。

--- luci-app-wrtbwmon/luasrc/model/cbi/wrtbwmon/config.lua
+++ luci-app-wrtbwmon/luasrc/model/cbi/wrtbwmon/config.lua
@@ -8,14 +8,25 @@ s = m:section(NamedSection, "general", "wrtbwmon", translate("General settings")
 o = s:option(Flag, "enabled", translate("Enabled"))
 o.rmempty= true
 
-bandwidth = s:option( Value, "bdw", translate("Default bandwidth"))
+bandwidth = s:option( Value, "bdwup", translate("Upload bandwidth"))
 bandwidth:value("1M")
 bandwidth:value("10M")
 bandwidth:value("100M")
 bandwidth:value("200M")
 bandwidth:value("500M")
 bandwidth:value("1000M")
-bandwidth.default = '10M'
+bandwidth.default = '3M'
+
+bandwidth = s:option( Value, "bdwdown", translate("Download bandwidth"))
+bandwidth:value("1M")
+bandwidth:value("10M")
+bandwidth:value("100M")
+bandwidth:value("200M")
+bandwidth:value("500M")
+bandwidth:value("1000M")
+bandwidth:value("2500M")
+bandwidth:value("10000M")
+bandwidth.default = '100M'
 
 o = s:option(Value, "path", translate("Database Path"),
 	translate("This box is used to select the Database path, "
