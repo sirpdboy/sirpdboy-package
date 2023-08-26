6951168889aebf44b39f593470b7442f1ce7f1ba
SirPdboy <63764630+sirpdboy@users.noreply.github.com>
2023-08-26 17:54:28 +0800

    DEL

--- luci-app-control-speedlimit/luasrc/controller/speedlimit.lua
+++
@@ -1,9 +0,0 @@
-module("luci.controller.speedlimit", package.seeall)
-
-function index()
-	if not nixio.fs.access("/etc/config/speedlimit") then return end
-	local page
-        entry({"admin", "control"}, firstchild(), "Control", 44).dependent = false
-	page = entry({"admin","control","speedlimit"},cbi("speedlimit"),_("网速限制"),67)
-	page.dependent = true
- end
